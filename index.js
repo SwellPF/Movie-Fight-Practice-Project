e5b3de11d26e844ceb6b11b33dca5ada8c89e095
Paul Fell <swellpf@gmail.com>
2021-09-08 16:45:52 -0400

    added axios API call logic

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '81b387fe',
+            s: searchTerm
+        }
+    } );
+
+    console.log(response.data);
+};
+
+const input = document.querySelector('input');
+let timeoutID;
+const onInput = event => {
+    if(timeoutID) {
+        clearTimeout(timeoutID);
+    }
+    timeoutID = setTimeout(() => {fetchData(event.target.value);
+    }, 1000)
+};
+
+input.addEventListener('input', onInput);
